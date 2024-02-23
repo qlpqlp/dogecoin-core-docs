082e3d915f5fe4f56c1485867c581edf1205cdd6
inevitable360 <pvidalx@gmail.com>
2024-02-23 14:13:48 +0000

    Check if it fixes SUMMARY.md
    Check if it fixes SUMMARY.md

--- SUMMARY_generate.py
+++ SUMMARY_generate.py
@@ -19,8 +19,21 @@ def create_summary():
             # Add entry to the summary
             summary_content += f"- [{lang_folder.upper()}]({readme_path})\n"
 
+        # Check for the existence of a 'doc' folder inside the language folder
+        doc_folder_path = os.path.join("src", lang_folder, "doc")
+        if os.path.exists(doc_folder_path) and os.path.isdir(doc_folder_path):
+            # Add 'Development' menu item
+            summary_content += "\n\n"
+            summary_content += "# Development\n"
+            # Loop through .md files in the 'doc' folder
+            for doc_file in os.listdir(doc_folder_path):
+                if doc_file.endswith(".md"):
+                    # Add sub-menu item for each .md file
+                    file_name = os.path.splitext(doc_file)[0]
+                    summary_content += f"- [{file_name}]({lang_folder}/doc/{doc_file})\n"
+
     # Write summary content to SUMMARY.md file
-    with open("SUMMARY.md", "w") as summary_file:
+    with open("src/SUMMARY.md", "w") as summary_file:
         summary_file.write(summary_content)
 
 if __name__ == "__main__":
